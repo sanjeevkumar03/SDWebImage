ffb8d5aea41a4f371431476a5fa857033ad6cb37
sanjeevkumar03 <91119044+sanjeevkumar03@users.noreply.github.com>
2025-04-30 21:38:11 +0530

    Update Package.swift
    added type: static

--- Package.swift
+++ Package.swift
@@ -15,9 +15,11 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SDWebImage",
+            type: .static,
             targets: ["SDWebImage"]),
         .library(
             name: "SDWebImageMapKit",
+            type: .static,
             targets: ["SDWebImageMapKit"])
     ],
     dependencies: [
@@ -31,7 +33,6 @@ let package = Package(
             name: "SDWebImage",
             dependencies: [],
             path: "SDWebImage",
-            type: .static,
             sources: ["Core", "Private"],
             resources: [.copy("Resources/PrivacyInfo.xcprivacy")],
             cSettings: [
